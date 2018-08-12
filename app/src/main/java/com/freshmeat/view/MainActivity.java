f9bc618715333bb5fbd3e62a6a8bbed891c2ceec
Mahdi <mahdikeshavarz903@gmail.com>
2018-08-13 02:46:35 +0430

    configuration

--- app/src/main/java/com/freshmeat/view/MainActivity.java
+++ app/src/main/java/com/freshmeat/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.freshmeat;
+package com.freshmeat.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.freshmeat.R;
 
 public class MainActivity extends AppCompatActivity {
 
