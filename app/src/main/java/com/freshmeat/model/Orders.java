caa550f251e9c7eca4d04a34b84b682bd1e2ad8a
Mahdi <mahdikeshavarz903@gmail.com>
2018-08-15 11:55:47 +0430

    complete model

---
+++ app/src/main/java/com/freshmeat/model/Orders.java
@@ -0,0 +1,4 @@
+package com.freshmeat.model;
+
+public class Orders {
+}
--- app/src/main/java/com/freshmeat/view/activity/MainActivity.java
+++ app/src/main/java/com/freshmeat/view/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.freshmeat.view;
+package com.freshmeat.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
